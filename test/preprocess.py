9dacc8c33c5e50825ae231f590660a9f31a2f061
JingYu Ning <foldfelis@gmail.com>
2020-01-29 17:53:28 +0800

    refactor preprocess test script

--- test/preprocess.py
+++ test/preprocess.py
@@ -1,6 +1,6 @@
 from spinat import preprocess
 
-COL_NAMES_MOKE_2MODE = [
+COL_NAMES_MOKE_GROWTH_2MODE = [
         "Time_i", "FluxSum1_i", "FluxSum2_i",
         "MR_i", "MS_i", "MaxField_i",
         "Logic_i", "Temp_i", "Pressure_i",
@@ -10,17 +10,19 @@ COL_NAMES_MOKE_2MODE = [
         "Logic_o", "Temp_o", "Pressure_o",
 ]
 
+COL_NAMES_MOKE = ["Field", "Kerr"]
+
 
 def moke_growth_2mode_file_test():
     file = "./Demo/MOKE_Growth/4FeCu.bin"
-    df = preprocess.read_expr(file, COL_NAMES_MOKE_2MODE, skip_row=2, sep=" ")
+    df = preprocess.read_expr(file, COL_NAMES_MOKE_GROWTH_2MODE, skip_row=2, sep=" ")
 
     return df
 
 
 def moke_growth_partial_2mode_file_test():
     file = "./Demo/MOKE_Growth/4FeCu.bin"
-    col = COL_NAMES_MOKE_2MODE[:int(len(COL_NAMES_MOKE_2MODE)/2)]
+    col = COL_NAMES_MOKE_GROWTH_2MODE[:int(len(COL_NAMES_MOKE_GROWTH_2MODE) / 2)]
     df = preprocess.read_expr(file, col, skip_row=2, sep=" ")
 
     return df
@@ -33,8 +35,10 @@ def moke_growth_1mode_file_test():
         f"Time_{iop}",
         f"Flux1_{iop}", f"Flux2_{iop}",
         f"FluxSum1_{iop}", f"FluxSum2_{iop}",
-        f"MR_{iop}", f"MS_{iop}", f"DC_MR_{iop}", f"DC_MS_{iop}",
-        f"MaxField_{iop}", f"Logic_{iop}", f"Temp_{iop}", f"Pressure_{iop}",
+        f"MR_{iop}", f"MS_{iop}",
+        f"DC_MR_{iop}", f"DC_MS_{iop}",
+        f"MaxField_{iop}", f"Logic_{iop}",
+        f"Temp_{iop}", f"Pressure_{iop}",
     ]
     df = preprocess.read_expr(file, col_names, skip_row=2, sep=" ")
 
@@ -43,8 +47,7 @@ def moke_growth_1mode_file_test():
 
 def moke_ncue_file_test():
     file = "./Demo/MOKE/3FeCu_z62_P_200Oe.txt"
-    col_names = ["Field", "Kerr"]
-    df = preprocess.read_expr(file, col_names, skip_row=0, sep=" ")
+    df = preprocess.read_expr(file, COL_NAMES_MOKE, skip_row=0, sep=" ")
 
     return df
 
